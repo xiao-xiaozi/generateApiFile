237ec6b63a1f2b3543edd18676074192a3d1613f
小小子 <xiaoxiaozi_qiu@163.com>
2023-02-08 14:24:19 +0800

    fix: 注释对接口参数的解析
    1. 引入lodash，请求函数名获取调整

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-const fs = require('fs')
-const http = require('http')
-
+const fs = require("fs");
+const http = require("http");
+const _ = require('lodash')
 
 let setting = {
-  dir:'test-ApiFile',
-  swaggerUrl:'http://172.16.8.102:8529/salary/v2/api-docs',
+  dir: "test-ApiFile",
+  swaggerUrl: "http://172.16.8.102:8530/econtract/v2/api-docs",
   // swaggerUrl:'http://172.16.6.254:9010/userManage/v2/api-docs',
   // 如果swagger配置了访问账号则需要配置 auth
-  options:{
+  options: {
     // auth:'user:password'
-  }
-}
+  },
+};
 
-
-http.get(setting.swaggerUrl,setting.options,(res) => {
+http
+  .get(setting.swaggerUrl, setting.options, (res) => {
     const { statusCode } = res;
-    const contentType = res.headers['content-type'];
-  
+    const contentType = res.headers["content-type"];
+
     let error;
     // Any 2xx status code signals a successful response but
     // here we're only checking for 200.
     if (statusCode !== 200) {
-      error = new Error('Request Failed.\n' +`Status Code: ${statusCode}`);
+      error = new Error("Request Failed.\n" + `Status Code: ${statusCode}`);
     } else if (!/^application\/json/.test(contentType)) {
-      error = new Error('Invalid content-type.\n' +`Expected application/json but received ${contentType}`);
+      error = new Error(
+        "Invalid content-type.\n" +
+          `Expected application/json but received ${contentType}`
+      );
     }
     if (error) {
       console.error(error.message);
@@ -31,107 +34,131 @@ http.get(setting.swaggerUrl,setting.options,(res) => {
       res.resume();
       return;
     }
-    res.setEncoding('utf8');
-    let rawData = '';
-    res.on('data', (chunk) => { rawData += chunk; });
-    res.on('end', () => {
+    res.setEncoding("utf8");
+    let rawData = "";
+    res.on("data", (chunk) => {
+      rawData += chunk;
+    });
+    res.on("end", () => {
       try {
         const parsedData = JSON.parse(rawData);
-        generateApiFile(parsedData)
+        generateApiFile(parsedData);
       } catch (e) {
         console.error(e.message);
       }
     });
-}).on('error',(e)=> {
+  })
+  .on("error", (e) => {
     console.error(`Got error: ${e.message}`);
-})
-
-
-
+  });
 
 // api参数字符串
-function paramsFn(parameters,definitions){
-  let paramsStr = ''
-  parameters.forEach((param) => {
-    if(param.in === 'header' || param.in === 'path') return
-    if(Reflect.has(param,'schema')){
-      if(Reflect.has(param.schema,'originalRef')){
-        if(Reflect.has(definitions,param.schema.originalRef)){
-          let schema = definitions[param.schema.originalRef]
-          let {type,required,properties} = schema
-          if(type && type === 'object'){
-            if(properties) {
-              for(prop in properties) {
-                if(required && Array.isArray(required) && required.includes(prop)){
-                  paramsStr += backTemplate(prop,true,properties[prop].type,properties[prop].description)
-                }else {
-                  paramsStr += backTemplate(prop,false,properties[prop].type,properties[prop].description)
-                }
-              }
-            }
-          }
-        }
-      }
-    }else {
-      if(param.required){
-        paramsStr += backTemplate(param.name,true,param.type,param.description)
-      }else {
-        paramsStr += paramsStr += backTemplate(param.name,true,param.type,param.description)
-      }
-    }
-  })
+// function paramsFn(parameters, definitions) {
+//   let paramsStr = "";
+//   parameters.forEach((param) => {
+//     if (param.in === "header" || param.in === "path") return;
+//     if (Reflect.has(param, "schema")) {
+//       if (Reflect.has(param.schema, "originalRef")) {
+//         if (Reflect.has(definitions, param.schema.originalRef)) {
+//           let schema = definitions[param.schema.originalRef];
+//           let { type, required, properties } = schema;
+//           if (type && type === "object") {
+//             if (properties) {
+//               for (prop in properties) {
+//                 if (
+//                   required &&
+//                   Array.isArray(required) &&
+//                   required.includes(prop)
+//                 ) {
+//                   paramsStr += backTemplate(
+//                     prop,
+//                     true,
+//                     properties[prop].type,
+//                     properties[prop].description
+//                   );
+//                 } else {
+//                   paramsStr += backTemplate(
+//                     prop,
+//                     false,
+//                     properties[prop].type,
+//                     properties[prop].description
+//                   );
+//                 }
+//               }
+//             }
+//           }
+//         }
+//       }
+//     } else {
+//       if (param.required) {
+//         paramsStr += backTemplate(
+//           param.name,
+//           true,
+//           param.type,
+//           param.description
+//         );
+//       } else {
+//         paramsStr += paramsStr += backTemplate(
+//           param.name,
+//           true,
+//           param.type,
+//           param.description
+//         );
+//       }
+//     }
+//   });
 
-  function backTemplate(name,required,type,description){
-    return `
-* @name:${name}
-* @required:${required}
-* @type:${type}
-* @description:${description}
-`
-}
-  return paramsStr
-}
+//   function backTemplate(name, required, type, description) {
+//     return `
+// * @name:${name}
+// * @required:${required}
+// * @type:${type}
+// * @description:${description}
+// `;
+//   }
+//   return paramsStr;
+// }
 
-
-function generateApiFile(apiDoc){
-  let { paths, definitions, tags} = apiDoc
-  if(!paths) throw new Error('paths field is undefined!')
-  if(!definitions) throw new Error('definitions field is undefined!')
-  if(!tags) throw new Error('tags field is undefined!')
+function generateApiFile(apiDoc) {
+  let { paths, definitions, tags } = apiDoc;
+  if (!paths) throw new Error("paths field is undefined!");
+  if (!definitions) throw new Error("definitions field is undefined!");
+  if (!tags) throw new Error("tags field is undefined!");
 
   // 接口类型/标签/标识
-  let tagObj = {}
-  tags.forEach(tag => {
+  let tagObj = {};
+  tags.forEach((tag) => {
     tagObj[tag.name] = {
-      name:tag.name,
-      apiStr:''
-    }
-  })
+      name: tag.name,
+      apiStr: "",
+    };
+  });
 
   // paths 处理
-  for(let url in paths) {
-    for(let method in paths[url]) {
-      let apiInfo = paths[url][method] // 接口信息
+  for (let url in paths) {
+    for (let method in paths[url]) {
+      let apiInfo = paths[url][method]; // 接口信息
       // apiDescription
-      let apiDescription = apiInfo.summary
+      let apiDescription = apiInfo.summary;
       // params 参数
-      let apiParams = paramsFn(apiInfo.parameters,definitions);
+      // let apiParams = paramsFn(apiInfo.parameters, definitions);
 
       // todo：函数名取接口split('/')后的哪一段
-      let urlSplit = url.split('/')
-      tagObj[apiInfo.tags[0]].name = urlSplit[1] // api js 文件名
-      let parametersIn = apiInfo.parameters.map(item => item.in) // 接口参数位置
+      let urlSplit = url.split("/");
+      tagObj[apiInfo.tags[0]].name = urlSplit[1]; // api js 文件名
+      let parametersIn = apiInfo.parameters.map((item) => item.in); // 接口参数位置
       let fnName; // 从接口路径获取api函数名
       let apiUrl;
-      if(parametersIn.includes('path')) { // 接口参数在请求路径上
-        fnName = urlSplit[urlSplit.length - 2]
-        let index = url.indexOf('{')
-        apiUrl = url.slice(0,index)
-        let pathParam = apiInfo.parameters.find(el => el.in === 'path')
-        tagObj[apiInfo.tags[0]].apiStr += (`
+      if (parametersIn.includes("path")) {
+        // 接口参数在请求路径上
+        // fnName = urlSplit[urlSplit.length - 2];
+        fnName = _.camelCase(urlSplit[urlSplit.length - 3] + ' ' + urlSplit[urlSplit.length - 2])
+        let index = url.indexOf("{");
+        apiUrl = url.slice(0, index);
+        let pathParam = apiInfo.parameters.find((el) => el.in === "path");
+        tagObj[apiInfo.tags[0]].apiStr += `
 /**
  * @description ${apiDescription}
- * ${apiParams}
 */
 export function ${fnName}(${pathParam.name}) {
   return request({
@@ -139,57 +166,60 @@ export function ${fnName}(${pathParam.name}) {
     method: '${method}',
   })
 }
-`)
-      }else {
-        fnName = urlSplit[urlSplit.length - 1]
-        apiUrl = url
-        tagObj[apiInfo.tags[0]].apiStr += (`
+`;
+      } else {
+        // fnName = urlSplit[urlSplit.length - 1];
+        fnName = _.camelCase(urlSplit[urlSplit.length - 2] + ' ' + urlSplit[urlSplit.length - 1]);
+        apiUrl = url;
+        tagObj[apiInfo.tags[0]].apiStr += `
 /**
  * @description ${apiDescription}
- * ${apiParams}
 */
-export function ${fnName}(${method =='get'?'params':'data'}) {
+export function ${fnName}(${method == "get" ? "params" : "data"}) {
   return request({
     url: '${apiUrl}',
     method: '${method}',
-    ${method =='get'?'params':'data'}
+    ${method == "get" ? "params" : "data"}
   })
 }
-`)
+`;
       }
     }
   }
 
-  saveApiFile(tagObj)
+  saveApiFile(tagObj);
 }
 
-
-function saveApiFile(tagObj){
-  let files
+function saveApiFile(tagObj) {
+  let files;
   // 如果有这个文件夹
   if (fs.existsSync(setting.dir)) {
-    files = fs.readdirSync(setting.dir)
+    files = fs.readdirSync(setting.dir);
     files.forEach((file) => {
-      const curPath = `${setting.dir}/${file}`
+      const curPath = `${setting.dir}/${file}`;
       if (fs.statSync(curPath).isDirectory()) {
         // 递归删除文件夹
-        folderObj.delDir(curPath)
+        folderObj.delDir(curPath);
       } else {
         // 删除文件
-        fs.unlinkSync(curPath)
+        fs.unlinkSync(curPath);
       }
-    })
-  }else {
-    fs.mkdirSync(setting.dir)
+    });
+  } else {
+    fs.mkdirSync(setting.dir);
   }
-  for(prop in tagObj) {
-    fs.writeFileSync('./'+setting.dir+'/'+tagObj[prop].name+'.js',tagObj[prop].apiStr,{
-        encoding:'utf-8'
-    })
+  for (prop in tagObj) {
+    fs.writeFileSync(
+      "./" + setting.dir + "/" + tagObj[prop].name + ".js",
+      tagObj[prop].apiStr,
+      {
+        encoding: "utf-8",
+      }
+    );
   }
 }
 
 // for in 遍历 paths 获得请求地址 url
 // paths[url] 进行 for in 得到请求类型 method
-// paths[url][method]  .summary 接口注释信息 url.split('/')[leng-1] 得到函数名字 
+// paths[url][method]  .summary 接口注释信息 url.split('/')[length-1] 得到函数名字
 // definitions 存储着参数信息 可以根据paths[url].method.parameters[0].schema.originalRef 去definitions中获取
